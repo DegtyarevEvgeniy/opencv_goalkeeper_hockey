384ddcd514a483ddfe884a88febca4ee2f7a2a46
doctorDim <nebuhai@gmail.com>
2019-04-28 17:58:51 +0300

    add range rgb

--- goalkeeper.py
+++ goalkeeper.py
@@ -5,10 +5,52 @@ import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
 
-ap = argparse.ArgumentParser(description='Хоккейный вратарь. Отбивает шайбы по льду.')
+ap = argparse.ArgumentParser(description='Хоккейный вратарь. Отбивает шайбы по льду. При условии. что шайба движется по прямой.')
 ap.add_argument("-c", "--coordinat", type = int, required = False, help="Вывод изображения на координатной оси.")
 ap.add_argument("-p", "--predict", type = int, required = False, help="Предсказание для введенного числа X.")
+ap.add_argument("-r", "--range", type = int, required = False, help="Определение параметров RGB.")
 args = vars(ap.parse_args())
+
+# Калибровка по цвету
+def range(camera):
+
+    def nothing(x):
+        pass
+
+    cap = cv2.VideoCapture(camera)
+    cv2.namedWindow('result')
+
+    cv2.createTrackbar('minb', 'result', 0, 255, nothing)
+    cv2.createTrackbar('ming', 'result', 0, 255, nothing)
+    cv2.createTrackbar('minr', 'result', 0, 255, nothing)
+
+    cv2.createTrackbar('maxb', 'result', 0, 255, nothing)
+    cv2.createTrackbar('maxg', 'result', 0, 255, nothing)
+    cv2.createTrackbar('maxr', 'result', 0, 255, nothing)
+
+    while(cap.isOpened()):
+        _, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow('hsv', hsv)
+
+        minb = cv2.getTrackbarPos('minb', 'result')
+        ming = cv2.getTrackbarPos('ming', 'result')
+        minr = cv2.getTrackbarPos('minr', 'result')
+
+        maxb = cv2.getTrackbarPos('maxb', 'result')
+        maxg = cv2.getTrackbarPos('maxg', 'result')
+        maxr = cv2.getTrackbarPos('maxr', 'result')
+
+        mask = cv2.inRange(hsv, (minb,ming,minr), (maxb,maxg, maxr))
+        cv2.imshow('mask', mask)
+        result = cv2.bitwise_and(frame, frame, mask = mask)
+        cv2.imshow('result', result)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 # Линейная регрессия
 def predict(X):
@@ -60,3 +102,6 @@ if __name__ == '__main__':
 
     if args["predict"] is not None:
         predict(args["predict"])
+
+    if args["range"] is not None:
+        range(args["range"])
